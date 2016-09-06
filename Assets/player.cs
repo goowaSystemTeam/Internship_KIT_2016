b769b2fec3203ed494e1b9e7530a305820f82453
hajime620 <7zwwhosus7>
2016-09-06 15:39:57 +0900

    ステージ2
    ステージ2の廊下とドアのアニメーション

---
+++ Assets/player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	public float  jumpPower;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			GetComponent<Rigidbody> ().velocity = new Vector3 (0, jumpPower, 0);
+		}
+	}
+	void OnCollisionEnter(Collision other)
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
