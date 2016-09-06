4f2c0bb78b04ffd8e1b161be411ea13c97013285
hajime620 <7zwwhosus7>
2016-09-06 15:41:40 +0900

    ステージ2
    ステージ2の続き

---
+++ Assets/DoorOp.js
@@ -0,0 +1,13 @@
+﻿private var isTriggered = false;
+
+function OnTriggerEnter(other : collider)
+{
+if(other.gameObject.tag == "player"){
+isTriggered =true;
+}
+}
+
+function OnTriggerExit(collider : collider)
+{
+isTriggered = false;
+}
