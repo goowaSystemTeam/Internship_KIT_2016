f212a84056ebca7d2a75247b272a88338889de8b
629OKAI <intern@Robin-no-MacBook-Pro.local>
2016-09-06 15:37:49 +0900

    main画面
    メイン画面の装飾、ボタンの設置

---
+++ Assets/script/mainloadscript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public void LoadMain() {
+		Application.LoadLevel ("main");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
