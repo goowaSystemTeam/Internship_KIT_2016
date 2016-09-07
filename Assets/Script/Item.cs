3e54d9d2593c6b7a48006671f4e7b5659072cb22
TauchiYuki <intern@Sengoku-no-MacBook-Air.local>
2016-09-07 16:05:16 +0900

    インターン3日目
    一人称視点のマウス操作追加
    銃のモデルをカメラ上に追加
    弾丸発射のスクリプトを追加
    弾丸は３秒立つと消えるように

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+	public float destroyTime = 3;
+
+	//このオブジェクト(Bullet)が生成された瞬間に実行される。
+	void Start(){
+		//このオブジェクトをdestroyTimeで指定した秒後に削除する。
+		Destroy(gameObject, destroyTime);
+	}
+
+	// トリガーとの接触時に呼ばれるコールバック
+	/*void OnTriggerEnter (Collider hit)
+	{
+		// このコンポーネントを持つGameObjectを破棄する
+		Destroy(gameObject);
+	}*/
+}
---
+++ Assets/Script/Itemtana.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Itemtana : MonoBehaviour
+{
+	// トリガーとの接触時に呼ばれるコールバック
+	void OnTriggerEnter (Collider hit)
+	{
+		// 接触対象はPlayerタグですか？
+		if (hit.CompareTag ("ballet")) {
+
+			// このコンポーネントを持つGameObjectを破棄する
+			Destroy(gameObject);
+		}
+	}
+}
