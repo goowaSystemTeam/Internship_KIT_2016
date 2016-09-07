53818462d649837fa5cc39654822a24c0013d755
hajime620 <7zwwhosus7>
2016-09-07 16:02:19 +0900

    3日目あたり判定
    やばいです!!

---
+++ Assets/DoorController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+	public float openSpeed; //ドアオープンスピード
+	private bool doorOpen; //ドアチェック
+	private float yDegree; //ドア回転角
+
+	void Start()
+	{
+		doorOpen = false;
+		yDegree = 0.0F;
+	}
+	void Update()
+	{
+		if (doorOpen)
+		{
+			if (yDegree<90.0F)
+			{
+				yDegree += openSpeed * Time.deltaTime;
+				transform.Rotate (0,openSpeed * Time.deltaTime,0);
+			}
+		}
+		else
+		{
+			if (yDegree> 0.0F)
+			{
+				yDegree -= openSpeed * Time.deltaTime;
+				transform.Rotate (0,-openSpeed * Time.deltaTime,0);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		doorOpen =true;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		doorOpen = false;
+	}
+}
