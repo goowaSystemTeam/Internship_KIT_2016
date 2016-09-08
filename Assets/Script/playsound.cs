0cada50d0ce0b27bbdf976736c019f1be762176b
TauchiYuki <intern@Sengoku-no-MacBook-Air.local>
2016-09-08 14:06:27 +0900

    4日目
    効果音とエネミーの追加

---
+++ Assets/Script/playsound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playsound : MonoBehaviour {
+
+	private AudioSource sound01;
+
+	// Use this for initialization
+	void Start () {
+
+		sound01 = GetComponent<AudioSource> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+
+			sound01.PlayOneShot (sound01.clip);
+
+		}
+	
+	}
+}
